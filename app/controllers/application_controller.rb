7e27d4b22837755e840a5fde596608c75e8d12e5
benbeyerlein <benbeyerlein@gmail.com>
2014-05-31 13:26:08 -0500

    Ready to customize devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :avatar_url
+
+    devise_parameter_sanitizer.for(:account_update) << :username
+    devise_parameter_sanitizer.for(:account_update) << :avatar_url
+  end
 end
